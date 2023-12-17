155efe111293d42ab758819edf4a0cb55c742178
KID <50666207+KID421@users.noreply.github.com>
2023-12-17 12:06:55 +0800

    Update 添加生成系統

--- Assets/Scripts/SpawnSystem.cs
+++ Assets/Scripts/SpawnSystem.cs
@@ -47,14 +47,61 @@ namespace KID
 
         [Header("放生成物位置")]
         public Transform spawnPoint;
+        [Header("下一個物件的位置")]
+        public Transform nextPoint;
+        [Header("放下按鍵")]
+        public KeyCode releaseSlimeKey = KeyCode.Space;
 
         private void Awake()
         {
-            int random = Random.Range(0, prefabSlimes.Length);
-            currentSlime = prefabSlimes[random];
+            currentSlime = RandomSlime();
+            nextSlime = RandomSlime();
 
-            int randomNext = Random.Range(0, prefabSlimes.Length);
-            nextSlime = prefabSlimes[randomNext];
+            // 生成(生成物件，座標，角度，父物件)
+            currentSlime = Instantiate(currentSlime, spawnPoint.position, Quaternion.identity, spawnPoint);
+            nextSlime = Instantiate(nextSlime, nextPoint.position, Quaternion.identity, nextPoint);
+        }
+
+        private void Update()
+        {
+            ReleaseSlime();
+        }
+
+        /// void 無傳回：使用這個方法不會有傳回資料
+        /// <summary>
+        /// 獲得隨機史萊姆
+        /// </summary>
+        /// <returns>隨機史萊姆</returns>
+        private GameObject RandomSlime()
+        {
+            int random = Random.Range(0, prefabSlimes.Length);
+            return prefabSlimes[random];
+        }
+
+        private void ReleaseSlime()
+        {
+            // 如果玩家按下放下按鍵 就放開始史萊姆(重力 1)
+            bool slimeKey = Input.GetKeyDown(releaseSlimeKey);
+            print($"<color=#f89>玩家有沒有按放下鍵：{slimeKey}</color>");
+
+            // 判斷式 if 語法
+            // if (布林值) { 程式，當布林值為 true 執行這裡 }
+            if (slimeKey)
+            {
+                // 史萊姆的重力設定為 1
+                // 目前史萊姆 取得他的 2D 剛體 並且將 重力 改為 1
+                currentSlime.GetComponent<Rigidbody2D>().gravityScale = 1;
+                // 將目前史萊姆的父物件設為空
+                currentSlime.transform.SetParent(null);
+                // 對調目前與下一隻
+                SwitchCurrentAndNext();
+            }
+        }
+
+        private void SwitchCurrentAndNext()
+        {
+            currentSlime = nextSlime;
+            nextSlime = RandomSlime();
         }
     }
 }
