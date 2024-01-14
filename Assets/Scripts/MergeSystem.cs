aea688bfe78622029ee81566cae279bf7bb8c7ac
KID <50666207+KID421@users.noreply.github.com>
2024-01-14 12:00:43 +0800

    Update 合成系統尚未完成

---
+++ Assets/Scripts/MergeSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 合成系統
+    /// </summary>
+    // 腳本執行順序，數字越小越先執行
+    [DefaultExecutionOrder(0)]
+    public class MergeSystem : MonoBehaviour
+    {
+        [Header("所有史萊姆預製物")]
+        public GameObject[] prefabSlimes;
+
+        // 單例模式
+        // 只有一個存在的時候可以使用
+        // instance 實體：物件
+        // static 靜態
+        public static MergeSystem instance;
+
+        private void Awake()
+        {
+            // 實體物件 = 這個腳本；
+            instance = this;
+        }
+
+        /// <summary>
+        /// 合成
+        /// </summary>
+        /// 小括號內參數語法：類型 參數名稱 (接收資料)
+        public void Merge(int _index)
+        {
+            print("<color=#99f>合成</color>");
+            Instantiate(prefabSlimes[_index], Vector3.zero, Quaternion.identity);
+        }
+    }
+}
--- Assets/Scripts/SlimeSystem.cs
+++ Assets/Scripts/SlimeSystem.cs
@@ -7,6 +7,9 @@ namespace KID
     /// </summary>
     public class SlimeSystem : MonoBehaviour
     {
+        [Header("史萊姆編號"), Range(0, 7)]
+        public int index;
+
         // OnCollisionXXX 碰撞事件
         // 條件
         // 1. 兩個物件都要有碰撞器 Collider
@@ -18,7 +21,7 @@ namespace KID
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            print($"<color=#f69>碰到的物件 {collision.gameObject.name}</color>");
+            // print($"<color=#f69>碰到的物件 {collision.gameObject.name}</color>");
 
             // 如果 碰到物件的名稱 與 這個物件的名稱 相同
             // 就 合成 (刪除、爆炸特效、升級後的史萊姆)
@@ -26,6 +29,13 @@ namespace KID
             // gameObject 此物件
             if (collision.gameObject.name == gameObject.name)
             {
+                print($"<color=#69f>要生出的史萊姆編號 {index + 1}</color>");
+
+                // 單例模式的使用方式
+                // 語法：
+                // 腳本名稱.實體.公開 public 成員
+                MergeSystem.instance.Merge(index + 1);
+
                 // 刪除物件(要刪除的物件)
                 Destroy(gameObject);
             }
